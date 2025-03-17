f6afd6d945e4b596c94c3a2ed50df6551a71d043
Fyz-dev <vladbutek@gmail.com>
2025-03-17 14:46:58 +0200

    refactor: models and entity

---
+++ qwitix-api/Models/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace qwitix_api.Models
+{
+    public class Ticket : BaseModel
+    {
+        [BsonRequired]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("event_id")]
+        public string EventId { get; set; } = null!;
+
+        [BsonRequired]
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("details")]
+        public string Details { get; set; } = null!;
+
+        [BsonRequired]
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+
+        [BsonRequired]
+        [BsonElement("quantity")]
+        public int Quantity { get; set; }
+
+        [BsonElement("sold")]
+        public int Sold { get; set; } = 0;
+    }
+}
---
+++ qwitix-api/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using qwitix_api.Core.Enums;
+
+namespace qwitix_api.Models
+{
+    public class Transaction : BaseModel
+    {
+        [BsonRequired]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("user_id")]
+        public string UserId { get; set; } = null!;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("ticket_id")]
+        public string TicketId { get; set; } = null!;
+
+        [BsonRequired]
+        [BsonElement("amount")]
+        public decimal Amount { get; set; }
+
+        [BsonRequired]
+        [BsonElement("currency")]
+        public string Currency { get; set; } = "USD";
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("status")]
+        public TransactionStatus Status { get; set; } = TransactionStatus.Pending;
+
+        [BsonElement("stripe_payment_id")]
+        public string StripePaymentId { get; set; } = string.Empty;
+    }
+}
