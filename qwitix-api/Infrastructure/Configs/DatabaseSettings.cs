6b90f72ac72a25353f4956b3a32e55170355bded
Fyz-dev <vladbutek@gmail.com>
2025-03-17 17:03:32 +0200

    feat: database config

---
+++ qwitix-api/Infrastructure/Configs/DatabaseSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace qwitix_api.Infrastructure.Configs
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string UsersCollectionName { get; set; } = null!;
+
+        public string OrganizersCollectionName { get; set; } = null!;
+
+        public string EventsCollectionName { get; set; } = null!;
+
+        public string TicketsCollectionName { get; set; } = null!;
+
+        public string TransactionsCollectionName { get; set; } = null!;
+    }
+}
