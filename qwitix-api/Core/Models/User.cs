bcc71ca81879cc3c34e7e5d919b654099bda4aa8
Fyz-dev <vladbutek@gmail.com>
2025-04-02 18:38:48 +0300

    feat: user

--- qwitix-api/Core/Models/User.cs
+++ qwitix-api/Core/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using qwitix_api.Core.Enums;
 
@@ -10,26 +11,46 @@ namespace qwitix_api.Core.Models
         public string? RefreshToken { get; set; }
 
         [BsonElement("refresh_token_expires")]
+        [Required(ErrorMessage = "RefreshTokenExpires cannot be empty.")]
+        [DataType(DataType.DateTime)]
+        [CustomValidation(typeof(User), nameof(ValidateRefreshTokenExpires))]
         public DateTime? RefreshTokenExpires { get; set; }
 
         [BsonRequired]
         [BsonElement("google_id")]
+        [Required(ErrorMessage = "GoogleId cannot be empty.")]
         public string GoogleId { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("stripe_customer_id")]
+        [Required(ErrorMessage = "StripeCustomerId cannot be empty.")]
         public string StripeCustomerId { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("fullName")]
+        [Required(ErrorMessage = "FullName cannot be empty.")]
         public string FullName { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("email")]
+        [EmailAddress(ErrorMessage = "Email must follow the standard format (example@domain.com).")]
         public string Email { get; set; } = null!;
 
         [BsonRepresentation(BsonType.String)]
         [BsonElement("role")]
         public UserRole Role { get; set; } = UserRole.Customer;
+
+        private static ValidationResult? ValidateRefreshTokenExpires(
+            DateTime? refreshTokenExpires,
+            ValidationContext context
+        )
+        {
+            if (refreshTokenExpires.HasValue && refreshTokenExpires.Value < DateTime.Now)
+                return new ValidationResult(
+                    "RefreshTokenExpires cannot be earlier than the current date."
+                );
+
+            return ValidationResult.Success;
+        }
     }
 }
