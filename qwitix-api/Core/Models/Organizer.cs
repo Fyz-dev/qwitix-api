726c285f368655058da2c618bd68a003d60b5832
Fyz-dev <vladbutek@gmail.com>
2025-04-02 17:49:41 +0300

    feat: organizer properties

--- qwitix-api/Core/Models/Organizer.cs
+++ qwitix-api/Core/Models/Organizer.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace qwitix_api.Core.Models
@@ -8,17 +9,23 @@ namespace qwitix_api.Core.Models
         [BsonRequired]
         [BsonRepresentation(BsonType.ObjectId)]
         [BsonElement("user_id")]
+        [Required(ErrorMessage = "UserId cannot be empty")]
         public string UserId { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("name")]
+        [Required(ErrorMessage = "Name cannot be empty")]
+        [MaxLength(250, ErrorMessage = "Name cannot be longer than 250 characters")]
         public string Name { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("bio")]
+        [Required(ErrorMessage = "Bio cannot be empty")]
+        [MaxLength(2500, ErrorMessage = "Bio cannot be longer than 2500 characters")]
         public string Bio { get; set; } = null!;
 
         [BsonElement("image_url")]
+        [Url(ErrorMessage = "ImageUrl must be a valid URL")]
         public string? ImageUrl { get; set; }
 
         [BsonElement("is_verified")]
