7e05c9310059128621b978f83d75fcddcd2abced
Fyz-dev <vladbutek@gmail.com>
2025-04-04 17:59:37 +0300

    feat: create user mapper

---
+++ qwitix-api/Core/Mappers/UserMappers/CreateUserMapper.cs
@@ -0,0 +1,25 @@
+﻿using qwitix_api.Core.Models;
+using qwitix_api.Core.Services.UserService.DTOs;
+
+namespace qwitix_api.Core.Mappers.UserMappers
+{
+    public class CreateUserMapper : BaseMapper<CreateUserDTO, User>
+    {
+        public override CreateUserDTO ToDto(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override User ToEntity(CreateUserDTO dto)
+        {
+            return new User
+            {
+                GoogleId = dto.GoogleId,
+                StripeCustomerId = dto.StripeCustomerId,
+                FullName = dto.FullName,
+                Email = dto.Email,
+                Role = dto.Role,
+            };
+        }
+    }
+}
