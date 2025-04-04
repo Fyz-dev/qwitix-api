9bd2d12ca10cee001b200ff82f81133a6c196055
Fyz-dev <vladbutek@gmail.com>
2025-04-04 17:59:50 +0300

    feat: update user mapper

---
+++ qwitix-api/Core/Mappers/UserMappers/UpdateUserMapper.cs
@@ -0,0 +1,18 @@
+﻿using qwitix_api.Core.Models;
+using qwitix_api.Core.Services.UserService.DTOs;
+
+namespace qwitix_api.Core.Mappers.UserMappers
+{
+    public class UpdateUserMapper : BaseMapper<UpdateUserDTO, User>
+    {
+        public override User ToEntity(UpdateUserDTO dto)
+        {
+            return new User { FullName = dto.FullName, Email = dto.Email };
+        }
+
+        public override UpdateUserDTO ToDto(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
