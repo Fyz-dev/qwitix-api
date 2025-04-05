773d2b7fc258fde6b25f8426cab113528bddb2db
Fyz-dev <vladbutek@gmail.com>
2025-04-05 12:49:17 +0300

    feat: response transaction mapper

---
+++ qwitix-api/Core/Mappers/TransactionMappers/ResponseTransactionMapper.cs
@@ -0,0 +1,28 @@
+﻿using qwitix_api.Core.Models;
+using qwitix_api.Core.Services.TransactionService.DTOs;
+
+namespace qwitix_api.Core.Mappers.TransactionMappers
+{
+    public class ResponseTransactionMapper : BaseMapper<ResponseTransactionDTO, Transaction>
+    {
+        public override Transaction ToEntity(ResponseTransactionDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ResponseTransactionDTO ToDto(Transaction entity)
+        {
+            return new ResponseTransactionDTO
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                TicketId = entity.TicketId,
+                Amount = entity.Amount,
+                Currency = entity.Currency,
+                Status = entity.Status,
+                CreatedAt = entity.CreatedAt,
+                UpdatedAt = entity.UpdatedAt,
+            };
+        }
+    }
+}
