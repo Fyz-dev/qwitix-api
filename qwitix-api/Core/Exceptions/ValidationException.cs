e2ced7f24dd2f4ab871bd763bb9898e938981049
Fyz-dev <vladbutek@gmail.com>
2025-04-11 19:59:11 +0300

    feat: bad request

--- qwitix-api/Core/Exceptions/ValidationException.cs
+++ qwitix-api/Core/Exceptions/ValidationException.cs
@@ -1,4 +1,4 @@
 ﻿namespace qwitix_api.Core.Exceptions
 {
-    public class ValidationException(string message) : Exception(message);
+    public class ValidationException(string message) : ArgumentException(message);
 }
