62ca2a914beef9280013873a190d29b3673d2f20
Fyz-dev <vladbutek@gmail.com>
2025-04-11 19:36:28 +0300

    feat: ticket purchase mapper

---
+++ qwitix-api/Core/Mappers/TicketMappers/TicketPurchaseMapper.cs
@@ -0,0 +1,18 @@
+﻿using qwitix_api.Core.Models;
+using qwitix_api.Core.Services.TicketService.DTOs;
+
+namespace qwitix_api.Core.Mappers.TicketMappers
+{
+    public class TicketPurchaseMapper : BaseMapper<TicketPurchaseDTO, TicketPurchase>
+    {
+        public override TicketPurchase ToEntity(TicketPurchaseDTO dto)
+        {
+            return new TicketPurchase { TicketId = dto.TicketId, Quantity = dto.Quantity };
+        }
+
+        public override TicketPurchaseDTO ToDto(TicketPurchase entity)
+        {
+            return new TicketPurchaseDTO { TicketId = entity.TicketId, Quantity = entity.Quantity };
+        }
+    }
+}
