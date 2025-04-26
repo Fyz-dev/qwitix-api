9de4481d1d47aa460ada96ca79fa92b085c6fd38
Fyz-dev <vladbutek@gmail.com>
2025-04-26 15:18:28 +0300

    fix: type

--- qwitix-api/Core/Services/DTOs/UrlResponseDTO.cs
+++ qwitix-api/Core/Services/DTOs/UrlResponseDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace qwitix_api.Core.Services.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace qwitix_api.Core.Services.DTOs
 {
     public record UrlResponseDTO
     {
+        [Required]
         public required string Url { get; init; }
     }
 }
